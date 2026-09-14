7ae37ba579222992a0f47daad8bd78044ffae454
Paavni-590030140-B6 <replytopaavni@gmail.com>
2026-09-14 23:46:29 +0530

    Add files via upload

---
+++ day16_31.c
@@ -0,0 +1,21 @@
+//Write a program to take a number as input and print its equivalent binary representation.
+#include <stdio.h>
+
+int main()
+{
+    int n, binary = 0, rem, place = 1;
+
+    printf("Enter your number: ");
+    scanf("%d", &n);
+
+    while(n > 0)
+    {
+        rem = n % 2;
+        binary = binary + rem * place;
+        place = place * 10;
+        n = n / 2;
+    }
+
+    printf("Binary = %d", binary);
+    return 0;
+}
